f36afc6c3f622fee8078dec83676e2fd11394b28
Jason <65122962+jasoneinstien@users.noreply.github.com>
2022-05-25 01:54:22 +0800

    Add files via upload

--- vm/main.c
+++ vm/main.c
@@ -79,28 +79,82 @@ enum{
 //
 //main
 
-int main(int argc , const char* argv[]){
-    //load argument 
-    //set up 
-    
-    //since one condition flag should be set at any given time 
+int main(int argc, const char* argv[])
+{
+   // @{Load Arguments}
+    if(argc <2){
+
+    }
+   //@{Setup}
+
+    /* since exactly one condition flag should be set at any given time, set the Z flag */
     reg[R_COND] = FL_ZRO;
-     
-    //0x3000 is the default
-    enum {PC_START = 0x3000};
-    //set first data as start position 
-    reg[R_PC] = PC_START; 
+
+    /* set the PC to starting position */
+    /* 0x3000 is the default */
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START;
 
     int running = 1;
+    while (running)
+    {
+        /* FETCH */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
 
-    while (running){
-        uint16_t instr = mem_read(reg[R_PC] ++);
-        uint16_t op = instr >>12;
-    
+        switch (op)
+        {
+            case OP_ADD:
+               // @{ADD}
+                break;
+            case OP_AND:
+               // @{AND}
+                break;
+            case OP_NOT:
+              //  @{NOT}
+                break;
+            case OP_BR:
+               // @{BR}
+                break;
+            case OP_JMP:
+               // @{JMP}
+                break;
+            case OP_JSR:
+              //  @{JSR}
+                break;
+            case OP_LD:
+               // @{LD}
+                break;
+            case OP_LDI:
+               // @{LDI}
+                break;
+            case OP_LDR:
+              //  @{LDR}
+                break;
+            case OP_LEA:
+             //   @{LEA}
+                break;
+            case OP_ST:
+           //     @{ST}
+                break;
+            case OP_STI:
+         //       @{STI}
+                break;
+            case OP_STR:
+       //         @{STR}
+                break;
+            case OP_TRAP:
+     //           @{TRAP}
+                break;
+            case OP_RES:
+            case OP_RTI:
+            default:
+   //             @{BAD OPCODE}
+                break;
+        }
     }
-    restore_input_buffering();
+ //   @{Shutdown}
 }
-
 
 
 
